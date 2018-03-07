33406b6d56a0cc2805425bd57848e02ea0909b2b
Sean Wiseman <seanwiseman2012@gmail.com>
2018-03-07 15:33:42 +0000

    Let SDK handle ack when receiving message

--- events/python/receive_pika.py
+++ events/python/receive_pika.py
@@ -1,10 +1,13 @@
+import pika
+
 from setup_pika import (
     DASHBOARD_QUEUE_NAME,
     get_channel
 )
 
 
-def message_handler(ch, method, properties, body):
+def message_handler(channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
+                    properties: pika.spec.BasicProperties, body: str) -> None:
     print(f'[x] Received: {body}')
 
 
@@ -12,7 +15,7 @@ if __name__ == '__main__':
     with get_channel() as channel:
         print(' [*] Waiting for Messages. To exit press CTRL+C')
 
-        channel.basic_consume(message_handler, queue=DASHBOARD_QUEUE_NAME, no_ack=True)
+        channel.basic_consume(message_handler, queue=DASHBOARD_QUEUE_NAME, no_ack=False)
 
         try:
             channel.start_consuming()
