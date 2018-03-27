83f4c5515fe1bbb441ea8ad32385d5a05a379193
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-03-27 13:56:32 +0100

    Add basic articles

---
+++ api-dummy/src/Controller/ArticleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Libero\ApiDummy\Controller;
+
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use function is_dir;
+
+final class ArticleController
+{
+    private $dataPath;
+
+    public function __construct(string $dataPath)
+    {
+        $this->dataPath = $dataPath;
+    }
+
+    public function __invoke(Request $request, string $id, $version) : Response
+    {
+        if (!is_dir($articlePath = "{$this->dataPath}/articles/{$id}")) {
+            throw new NotFoundHttpException("Article {$id} not found");
+        }
+
+        if ('latest' === $version) {
+            $availableVersions = array_map('basename', glob("{$articlePath}/*", GLOB_ONLYDIR));
+            $version = max($availableVersions);
+        } else {
+            $version = (int) $version;
+        }
+
+        if (!is_dir($articlePath = "{$this->dataPath}/articles/{$id}")) {
+            throw new NotFoundHttpException("Article {$id} not found");
+        }
+        if (!is_dir($articleVersionPath = "{$articlePath}/{$version}")) {
+            throw new NotFoundHttpException("Article version {$version} not found");
+        }
+
+        $availableLanguages = array_map('basename', glob("{$articleVersionPath}/*", GLOB_ONLYDIR));
+        $language = $request->getPreferredLanguage($availableLanguages);
+
+        return new BinaryFileResponse("{$articleVersionPath}/{$language}/front.xml");
+    }
+}
