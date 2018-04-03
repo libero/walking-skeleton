afe225f8e9b589d3236d64a53d60884e224a3a71
Sean Wiseman <seanwiseman2012@gmail.com>
2018-04-03 14:01:10 +0100

    Cleaned up DAG

--- workflow/airflow/dags/foo_dag.py
+++ workflow/airflow/dags/foo_dag.py
@@ -1,10 +1,13 @@
+"""
+A simple DAG that has one operator which just prints its kwargs and return value
+to it's log then completes.
+"""
+
 from datetime import timedelta
 import pprint
-import time
 
 import airflow
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 
@@ -35,51 +38,15 @@ default_args = {
 }
 
 
-dag = DAG(dag_id='foo_python_operator', default_args=default_args, schedule_interval=None)
-
-
-# # t1, t2 and t3 are examples of tasks created by instantiating operators
-# t1 = BashOperator(
-#     task_id='print_date',
-#     bash_command='date',
-#     dag=dag)
-#
-# t1.doc_md = """\
-# #### Task Documentation
-# You can document your task using the attributes `doc_md` (markdown),
-# `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-# rendered in the UI's Task Instance Details page.
-# ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-# """
-#
-# dag.doc_md = __doc__
-
-
-def my_sleeping_function(random_base):
-    """This is a function that will run within the DAG execution"""
-    time.sleep(random_base)
-
-
-def print_context(ds, **kwargs):
+def print_context(*args, **kwargs):
     pprint.pprint(kwargs)
-    # print(ds)
-    # print(kwargs['params']['article_id'])
     article_id = kwargs['dag_run'].conf.get('article_id')
     return 'Ran foo task with article_id {}'.format(article_id)
 
 
-run_this = PythonOperator(task_id='print_the_context',
-                          provide_context=True,
-                          python_callable=print_context,
-                          # params={'article_id': article_id},
-                          dag=dag)
+foo_dag = DAG(dag_id='foo_python_operator', default_args=default_args, schedule_interval=None)
 
-# # Generate 10 sleeping tasks, sleeping from 0 to 9 seconds respectively
-# for i in range(10):
-#     task = PythonOperator(
-#         task_id='sleep_for_' + str(i),
-#         python_callable=my_sleeping_function,
-#         op_kwargs={'random_base': float(i) / 10},
-#         dag=dag)
-#
-#     task.set_upstream(run_this)
+foo_operator = PythonOperator(task_id='print_the_context',
+                              provide_context=True,
+                              python_callable=print_context,
+                              dag=foo_dag)
