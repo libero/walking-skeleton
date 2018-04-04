0cc4aaa6ee8098d7fd725c23530230992408dead
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-04-04 11:46:11 +0100

    Use Composer image

--- journal/.docker/scripts/install-composer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '"$(curl https://composer.github.io/installer.sig)"') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && \
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer && \
-php -r "unlink('composer-setup.php');"
