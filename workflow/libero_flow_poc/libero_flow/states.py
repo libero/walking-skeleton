3198da8534a67570dd32e0438ee829da0878be41
Sean Wiseman <seanwiseman2012@gmail.com>
2018-04-13 13:41:28 +0100

    Move states into own module

---
+++ workflow/libero_flow_poc/libero_flow/states.py
@@ -0,0 +1,9 @@
+
+
+CANCELLED = 'Cancelled'
+FINISHED = 'Finished'
+IN_PROGRESS = 'In Progress'
+PENDING = 'Pending'
+PERMANENT_FAILURE = 'Permanent Failure'
+SUCCEEDED = 'Succeeded'
+TEMPORARY_FAILURE = 'Temporary Failure'
