000c5524fc8609b4d95efe3ce60c7bcbee9460f2
Sean Wiseman <seanwiseman2012@gmail.com>
2018-04-19 08:35:13 +0100

    Add ingest article xml workflow

--- workflow/libero_flow_poc/libero_flow/activities/__init__.py
+++ workflow/libero_flow_poc/libero_flow/activities/__init__.py
@@ -1,4 +1,7 @@
 from libero_flow.activities.archive_xml_activity import ArchiveXMLActivity
+from libero_flow.activities.download_article_xml_activity import DownloadArticleXMLActivity
+from libero_flow.activities.download_assets_activity import DownloadAssetsActivity
+from libero_flow.activities.extract_asset_uris_activity import ExtractAssetURIsActivity
 from libero_flow.activities.clone_xml_activity import CloneXMLActivity
 from libero_flow.activities.generate_pdf_activity import GeneratePDFActivity
 
@@ -6,5 +9,8 @@ from libero_flow.activities.generate_pdf_activity import GeneratePDFActivity
 ACTIVITIES = {
     'ArchiveXMLActivity': ArchiveXMLActivity,
     'CloneXMLActivity': CloneXMLActivity,
-    'GeneratePDFActivity': GeneratePDFActivity
+    'DownloadArticleXMLActivity': DownloadArticleXMLActivity,
+    'DownloadAssetsActivity': DownloadAssetsActivity,
+    'ExtractAssetURIsActivity': ExtractAssetURIsActivity,
+    'GeneratePDFActivity': GeneratePDFActivity,
 }
---
+++ workflow/libero_flow_poc/libero_flow/activities/download_assets_activity.py
@@ -0,0 +1,35 @@
+import json
+import os
+import requests
+
+from libero_flow.activities.base_activity import BaseActivity
+
+
+class DownloadAssetsActivity(BaseActivity):
+
+    def do_activity(self) -> str:
+        """Download some static assets.
+
+        :return: str
+        """
+
+        input_data = self.session_get('input_data')
+        uris = self.session_get('uris')
+
+        if input_data and uris:
+            input_data = json.loads(input_data)
+            data_dir = input_data.get('data_dir')
+
+            uris = json.loads(uris.replace("'", '"'))
+
+            for uri in uris['uris']:
+                response = requests.get(url=uri)
+
+                file_name = uri.split('/')[-1]
+
+                with open(os.path.join(data_dir, file_name), 'wb') as asset_file:
+                    asset_file.write(response.content)
+
+            return self.SUCCEEDED
+
+        return self.PERMANENT_FAILURE
