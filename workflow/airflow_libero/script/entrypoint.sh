02a33b85af2ac1855415fb7f2ee57b7bf7cb650d
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-02 08:56:21 +0100

    Remove requirements.txt check in entrypoint.sh

--- workflow/airflow_libero/script/entrypoint.sh
+++ workflow/airflow_libero/script/entrypoint.sh
@@ -31,11 +31,6 @@ then
   AIRFLOW__CORE__LOAD_EXAMPLES=False
 fi
 
-# Install custom python package if requirements.txt is present
-if [ -e "/requirements.txt" ]; then
-    $(which pip) install --user -r /requirements.txt
-fi
-
 if [ -n "$REDIS_PASSWORD" ]; then
     REDIS_PREFIX=:${REDIS_PASSWORD}@
 else
