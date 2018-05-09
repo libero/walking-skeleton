617b8b41f9b2d0170b0ffb730d06ee6398bfe564
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-09 10:22:28 +0100

    Add ArticleVersion and Content models

--- article_store/articles/admin.py
+++ article_store/articles/admin.py
@@ -1,22 +1,41 @@
 from django.contrib import admin
 
-from articles.models import Article
+from articles.models import (
+    Article,
+    ArticleVersion,
+    Content,
+)
 
 
 class ArticleAdmin(admin.ModelAdmin):
 
     list_filter = (
         'id',
-        'status'
     )
 
     list_display = (
         'id',
-        'status',
-        'content',
+    )
+
+    list_display_links = ['id']
+
+
+class ContentAdmin(admin.ModelAdmin):
+
+    list_filter = (
+        'id',
+    )
+
+    list_display = (
+        'id',
+        'article_version',
+        'language',
+        'name',
     )
 
     list_display_links = ['id']
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleVersion)
+admin.site.register(Content, ContentAdmin   )
--- article_store/articles/models.py
+++ article_store/articles/models.py
@@ -14,9 +14,30 @@ ARTICLE_STATUSES = (
 
 class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    content = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=50, choices=ARTICLE_STATUSES, default=UNPUBLISHED)
-    # TODO version
 
     def __str__(self):
         return f'{self.id}'
+
+
+class ArticleVersion(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    article = models.ForeignKey(Article, related_name='versions', on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=ARTICLE_STATUSES, default=UNPUBLISHED)
+    version = models.IntegerField()
+
+    # TODO needs check on version number unique, e.g. can't save x2 version 1's
+
+    def __str__(self):
+        return f'Article: {self.article_id}, Version {self.version}'
+
+
+class Content(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    article_version = models.ForeignKey(ArticleVersion, related_name='content_items', on_delete=models.CASCADE)
+    language = models.CharField(max_length=50)
+    name = models.CharField(max_length=250)  # TODO has to be unique per version ??
+    text = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
