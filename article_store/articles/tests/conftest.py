a0260112bd65311c0f6bdfdd49e401e5dd0d8298
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-16 09:55:58 +0100

    User payload values in content tests

--- article_store/articles/tests/conftest.py
+++ article_store/articles/tests/conftest.py
@@ -28,7 +28,7 @@ def admin_rest_client(admin_user: Client) -> APIClient:
 @pytest.fixture
 @pytest.mark.django_db
 def article() -> Article:
-    return Article.objects.create()
+    return Article.objects.create(id='cc1250b5-0855-4fc2-906f-a6a77e4c90f5')
 
 
 @pytest.fixture(scope='session')
@@ -37,13 +37,28 @@ def article_0065_en_front_xml() -> str:
 
 
 @pytest.fixture(scope='session')
+def article_0065_en_front_payload_xml() -> str:
+    return parse_fixture_file('0065_en_front_payload.xml')
+
+
+@pytest.fixture(scope='session')
 def article_0065_es_front_xml() -> str:
     return parse_fixture_file('0065_es_front.xml')
 
 
 @pytest.fixture(scope='session')
+def article_0065_es_front_payload_xml() -> str:
+    return parse_fixture_file('0065_es_front_payload.xml')
+
+
+@pytest.fixture(scope='session')
 def article_0065_pt_front_xml() -> str:
     return parse_fixture_file('0065_pt_front.xml')
+
+
+@pytest.fixture(scope='session')
+def article_0065_pt_front_payload_xml() -> str:
+    return parse_fixture_file('0065_pt_front_payload.xml')
 
 
 @pytest.fixture
@@ -67,28 +82,53 @@ def article_version_3(article: Article) -> ArticleVersion:
 @pytest.fixture
 @pytest.mark.django_db
 def content_en_front(article_version_1: ArticleVersion,
-                     article_0065_en_front_xml: str) -> Content:
+                     article_0065_en_front_payload_xml: str) -> Content:
     return Content.objects.create(article_version=article_version_1,
                                   language='en',
                                   name='front',
-                                  text=article_0065_en_front_xml)
+                                  model='http://localhost:8081/schemas/scielo/article/front.rng',
+                                  content=article_0065_en_front_payload_xml)
 
 
 @pytest.fixture
 @pytest.mark.django_db
 def content_es_front(article_version_1: ArticleVersion,
-                     article_0065_es_front_xml: str) -> Content:
+                     article_0065_es_front_payload_xml: str) -> Content:
     return Content.objects.create(article_version=article_version_1,
                                   language='es',
                                   name='front',
-                                  text=article_0065_es_front_xml)
+                                  model='http://localhost:8081/schemas/scielo/article/front.rng',
+                                  content=article_0065_es_front_payload_xml)
 
 
 @pytest.fixture
 @pytest.mark.django_db
 def content_pt_front(article_version_1: ArticleVersion,
-                     article_0065_pt_front_xml: str) -> Content:
+                     article_0065_pt_front_payload_xml: str) -> Content:
     return Content.objects.create(article_version=article_version_1,
                                   language='pt',
                                   name='front',
-                                  text=article_0065_pt_front_xml)
+                                  model='http://localhost:8081/schemas/scielo/article/front.rng',
+                                  content=article_0065_pt_front_payload_xml)
+
+
+@pytest.fixture
+def content_xml_data() -> str:
+    return '''
+    <root>
+        <content-list>
+            <list-item>
+                <language>en</language>
+                <name>front</name>
+                <model>http://localhost:8081/schemas/scielo/article/body.rng</model>
+                <content>
+                    <front xmlns="http://libero.pub" xmlns:scielo="http://scielo.org" xml:lang="en">
+                        <id>0065-1737-azm-31-03-0367</id>
+                        <title>Damage evaluation of (Lepidoptera: Tortricidae) (Heinrich),
+                            structural parameters and natural regeneration in Pinus nelsonii (Shaw) and
+                            P. cembroides ( Zucc.) in Miquihuana, Tamaulipas</title>
+                    </front>
+                </content>
+            </list-item>
+        </content-list>
+    </root>'''
--- article_store/articles/tests/test_models/test_content_model.py
+++ article_store/articles/tests/test_models/test_content_model.py
@@ -4,8 +4,9 @@ from articles.models import Content
 
 
 @pytest.mark.django_db
-def test_can_create_content(content_en_front: Content, article_0065_en_front_xml: str):
+def test_can_create_content(content_en_front: Content,
+                            article_0065_en_front_payload_xml: str):
     assert content_en_front
     assert content_en_front.language == 'en'
     assert content_en_front.name == 'front'
-    assert content_en_front.text == article_0065_en_front_xml
+    assert content_en_front.content == article_0065_en_front_payload_xml
