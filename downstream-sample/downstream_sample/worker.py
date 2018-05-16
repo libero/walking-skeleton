634f6f8a787c3853283b632a2c6e4f4b5388dbfe
Giorgio Sironi <g.sironi@elifesciences.org>
2018-05-16 14:44:10 +0100

    Move channel orchestration to Worker.start

--- downstream-sample/downstream_sample/worker.py
+++ downstream-sample/downstream_sample/worker.py
@@ -1,11 +1,28 @@
 import json
 import pika
 
-class Worker:
-    def __init__(self, work):
-        self._work = work
+from downstream_sample.settings import (
+    get_channel,
+    get_queue
+)
 
-    def message_handler(self, channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
+class Worker:
+    def __init__(self, queue_name, work):
+        self._work = work
+        self._queue_name = queue_name
+
+    def start(self):
+        with get_channel() as channel:
+            get_queue()
+            channel.basic_consume(self._message_handler, queue=DOWNSTREAM_QUEUE_NAME, no_ack=False)
+
+            try:
+                print('Consuming...')
+                channel.start_consuming()
+            except KeyboardInterrupt:
+                channel.stop_consuming()
+
+    def _message_handler(self, channel: pika.channel.Channel, method: pika.spec.Basic.Deliver,
                         properties: pika.spec.BasicProperties, body: str) -> None:
         print("message_handler start")
         self._work(json.loads(body))
--- downstream-sample/manage.py
+++ downstream-sample/manage.py
@@ -10,14 +10,5 @@ from downstream_sample.settings import (
 if __name__ == '__main__':
     print('Starting worker...')
     crossref = crossref.FakeCrossref()
-    worker = worker.Worker(crossref.push)
-    # move into worker.py
-    with get_channel() as channel:
-        get_queue()
-        channel.basic_consume(worker.message_handler, queue=DOWNSTREAM_QUEUE_NAME, no_ack=False)
-#
-        try:
-            print('Consuming...')
-            channel.start_consuming()
-        except KeyboardInterrupt:
-            channel.stop_consuming()
+    worker = worker.Worker(DOWNSTREAM_QUEUE_NAME, crossref.push)
+    worker.start()
