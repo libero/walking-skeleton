b077302d2299e9df85638349dffccb203b457e94
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-16 08:51:10 +0100

    Extend admin to include inlines

--- article_store/articles/admin.py
+++ article_store/articles/admin.py
@@ -7,35 +7,55 @@ from articles.models import (
 )
 
 
+class ArticleVersionInline(admin.StackedInline):
+    model = ArticleVersion
+    show_change_link = True
+    extra = 0
+
+
+class ContentInline(admin.StackedInline):
+    model = Content
+    show_change_link = True
+    extra = 0
+
+
 class ArticleAdmin(admin.ModelAdmin):
 
+    list_filter = ['id']
+    list_display = ['id']
+    readonly_fields = ['id']
+    list_display_links = ['id']
+    inlines = [ArticleVersionInline]
+
+
+class ArticleVersionAdmin(admin.ModelAdmin):
     list_filter = (
         'id',
+        'version',
+        'status',
     )
-
     list_display = (
         'id',
+        'article_id',
+        'version',
+        'status',
     )
-
     list_display_links = ['id']
+    inlines = [ContentInline]
 
 
 class ContentAdmin(admin.ModelAdmin):
 
-    list_filter = (
-        'id',
-    )
-
+    list_filter = ['id']
     list_display = (
         'id',
         'article_version',
         'language',
         'name',
     )
-
     list_display_links = ['id']
 
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(ArticleVersion)
-admin.site.register(Content, ContentAdmin   )
+admin.site.register(ArticleVersion, ArticleVersionAdmin)
+admin.site.register(Content, ContentAdmin)
