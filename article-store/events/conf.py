ff085d3e37b67d068dda744cb6785967c2c580f1
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-23 10:26:18 +0100

    Renamed ARTICLES_QUEUE to ARTICLES_DUMMY_QUEUE
    To still allow for testing

--- article-store/events/conf.py
+++ article-store/events/conf.py
@@ -7,11 +7,11 @@ RABBITMQ_URL = os.environ.get('RABBITMQ_URL')
 
 BROKER_PARAMS = pika.connection.URLParameters(RABBITMQ_URL, '')
 
-ARTICLES_QUEUE = os.environ.get('ARTICLES_QUEUE', 'articles')
+ARTICLES_DUMMY_QUEUE = os.environ.get('ARTICLES_DUMMY_QUEUE', 'articles-dummy')
 ARTICLES_EXCHANGE = os.environ.get('ARTICLES_EXCHANGE', 'articles')
 
 DEFAULT_QUEUES = {
-    ARTICLES_QUEUE: [
+    ARTICLES_DUMMY_QUEUE: [
         ARTICLES_EXCHANGE
     ],
 }
