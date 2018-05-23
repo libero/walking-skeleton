c7f973f6916f79dba76773e58472135e85120f06
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-23 10:21:19 +0100

    Fix: Add default RABBITMQ_URL string value if not present
    Without this the article-store-postgres service throws an error because it is not being provided the broker details

--- article-store/events/conf.py
+++ article-store/events/conf.py
@@ -5,7 +5,7 @@ import pika
 
 RABBITMQ_URL = os.environ.get('RABBITMQ_URL')
 
-BROKER_PARAMS = pika.connection.URLParameters(RABBITMQ_URL)
+BROKER_PARAMS = pika.connection.URLParameters(RABBITMQ_URL, '')
 
 ARTICLES_QUEUE = os.environ.get('ARTICLES_QUEUE', 'articles')
 ARTICLES_EXCHANGE = os.environ.get('ARTICLES_EXCHANGE', 'articles')
