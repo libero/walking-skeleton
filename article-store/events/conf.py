f69c0341db37a5e0d6693450d5996e3a7399faab
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-24 09:09:56 +0100

    Remove dummy article queue

--- article-store/events/conf.py
+++ article-store/events/conf.py
@@ -7,7 +7,6 @@ RABBITMQ_URL = os.environ.get('RABBITMQ_URL', '')
 
 BROKER_PARAMS = pika.connection.URLParameters(RABBITMQ_URL)
 
-ARTICLES_DUMMY_QUEUE = os.environ.get('ARTICLES_DUMMY_QUEUE', 'articles-dummy')
 ARTICLES_EXCHANGE = os.environ.get('ARTICLES_EXCHANGE', 'articles')
 
 
