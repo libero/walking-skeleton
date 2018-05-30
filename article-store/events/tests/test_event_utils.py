08ce53bb9a115bdfc1b89b19186dfb9840b32098
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-30 09:57:17 +0100

    Fix failing message based tests

--- article-store/events/tests/test_event_utils.py
+++ article-store/events/tests/test_event_utils.py
@@ -9,15 +9,14 @@ from events.utils import (
 
 
 def test_can_get_base_message():
-    base_msg = create_message(msg_type='foo.test', identifier='someId', data={})
+    run_id = '2753d3ee-63de-11e8-9add-0242ac130008'
+    base_msg = create_message(msg_type='foo.test', run_id=run_id)
+
     assert base_msg
     assert len(base_msg['eventId'])
     assert base_msg['happenedAt']
     assert base_msg['type'] == 'foo.test'
-    assert base_msg['data'] == {}
-    assert base_msg['aggregate']['service'] == 'article-store'
-    assert base_msg['aggregate']['name'] == 'article-version'
-    assert base_msg['aggregate']['identifier'] == 'someId'
+    assert base_msg['runId'] == run_id
 
 
 def test_can_get_channel():
@@ -37,8 +36,7 @@ def test_can_send_article_message(mock_get_channel: MagicMock):
     channel = MagicMock(name='channel')
     mock_get_channel.return_value = channel
     send_article_message(msg_type='test-msg',
-                         article_id='12345',
-                         article_version=1)
+                         run_id='12345')
 
     assert mock_get_channel.call_count == 2
     assert channel.__enter__().basic_publish.called
