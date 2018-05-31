4776a8c4d8c8409aa5ed973c38cd2befa3523d15
Sean Wiseman <seanwiseman2012@gmail.com>
2018-05-31 15:26:07 +0100

    Add article store airflow adapter

---
+++ article-store/articles/airflow_adapter.py
@@ -0,0 +1,33 @@
+import json
+
+from django.conf import settings
+import requests
+
+
+DAG_NAME = 'make_assets_public'
+AIRFLOW_DAGS_URL = f'http://{settings.AIRFLOW_API_HOST}:{settings.AIRFLOW_API_PORT}/api/experimental/dags'
+TRIGGER_DAG_URL = f'{AIRFLOW_DAGS_URL}/{DAG_NAME}/dag_runs'
+
+
+def start_article_dag(run_id: str, article_id: str, article_version_id: str, article_version: str) -> bool:
+    """
+
+    :param run_id: str
+    :param article_id: str
+    :param article_version_id: str
+    :param article_version: str
+    :return: bool
+    """
+
+    payload = {
+        "schedule": "None",
+        "conf": "{\"input_data\": "
+                "{ \"run_id\": \"%s\", "
+                "\"article_id\": \"%s\","
+                "\"article_version\": \"%s\","
+                "\"article_version_id\": \"%s\""
+                "}}" % (run_id, article_id, article_version, article_version_id)
+    }
+
+    response = requests.post(TRIGGER_DAG_URL, data=json.dumps(payload))
+    return response.status_code == 200
