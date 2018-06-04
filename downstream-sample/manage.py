71319310ad4a0b57a1c4b02f1081f4a3f07e4049
Giorgio Sironi <g.sironi@elifesciences.org>
2018-06-04 14:12:13 +0100

    Ensure creation of downstream-sample exchange on bootstrap

--- downstream-sample/manage.py
+++ downstream-sample/manage.py
@@ -1,13 +1,15 @@
 from downstream_sample import settings, worker, events, crossref
 from downstream_sample.settings import (
     DOWNSTREAM_QUEUE_NAME,
-    DOWNSTREAM_EXCHANGE_NAME
+    DOWNSTREAM_EXCHANGE_NAME,
+    ensure_exchange
 )
 
 
 # only one command supported
 if __name__ == '__main__':
     print('Starting worker...')
+    ensure_exchange(DOWNSTREAM_EXCHANGE_NAME)
     crossref = crossref.FakeCrossref(events.Events(DOWNSTREAM_EXCHANGE_NAME))
     worker = worker.Worker(DOWNSTREAM_QUEUE_NAME, crossref.push)
     worker.start()
